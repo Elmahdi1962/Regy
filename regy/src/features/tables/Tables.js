40b23006d302b6dba19b5e605648cf10bc13140e
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-06-24 14:45:12 +0100

    dropdown changes

--- regy/src/features/tables/Tables.js
+++ regy/src/features/tables/Tables.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectTables } from './tablesSlice';
 import TableItem from './TableItem';
@@ -6,10 +6,26 @@ import './tables.css';
 
 function Tables() {
   const tables = useSelector(selectTables);
+  const handleDropDownClick = (e) => {
+    e.preventDefault();
+    e.target.classList.toggle('activeDropDownBtn');
+    document.querySelector('.addTableBtnDropDown').classList.toggle('addTableBtnDropDownOpen');
+  };
 
   return (
     <div className='tablesContainer'>
-      <div className='addTableBtn-left'><span>+</span></div>
+      <button className='addTableBtn' >
+        <span onClick={handleDropDownClick}>+</span>
+        <div className="addTableBtnDropDown" onClick={(e)=>{e.stopPropagation();e.preventDefault();}}>
+          <form>
+            <label htmlFor="title">Title</label>
+            <input name="title" type="text"/>
+            <label htmlFor="columns">Columns</label>
+            <textarea name="columns" type="text"/>
+            <input type="submit" />
+          </form>
+        </div>
+      </button>
 
       <div className='tableItemsContainer'>
         {
