04685098b58d0329f49999da807280468c962c16
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-06-29 12:48:14 +0100

    add link and update styles in footer

--- regy/src/components/footer/Footer.js
+++ regy/src/components/footer/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './footer.css'
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
 
 function Footer() {
   return (
     <footer>
-      <small>&copy; Created By Elmahdi</small>
+      <small>&copy; Created By</small>
+      <div className='name'>Elmahdi
+        <a href="https://github.com/Elmahdi1962" target="_blank" className='githubLink links'><BsGithub /></a>
+        <a href="https://www.linkedin.com/in/elmahdi-mamoun-a74a1a1bb/" target="_blank" className='linkedinLink links'><BsLinkedin /></a>
+      </div>
     </footer>
   )
 }
