4e5480a69b0dd9afcd1d37b59caacf607f5e74da
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-06-30 11:10:13 +0100

    implement persistent storage using localstorage

--- regy/src/app/store.js
+++ regy/src/app/store.js
@@ -3,10 +3,18 @@ import tablesReducer from '../features/tables/tablesSlice';
 import rowsReducer from '../features/rows/rowsSlice';
 import selectionReducer from '../features/selection/selectionSlice';
 
+const initialState = localStorage.getItem('state') ? JSON.parse(localStorage.getItem('state')) : {};
+const storageSaver = store => next => action => {
+  next(action);
+  localStorage.setItem('state', JSON.stringify(store.getState()));
+};
+
 export const store = configureStore({
   reducer: {
     tables: tablesReducer,
     rows: rowsReducer,
     selections: selectionReducer
   },
+  preloadedState: initialState,
+  middleware: [storageSaver]
 });
